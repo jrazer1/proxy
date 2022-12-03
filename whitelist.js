70d0bacb03702e9df891c445115c428f79f566c0
jrazer1 <jrazer1@yandex.ru>
2022-12-03 08:32:43 +0300

    upd

---
+++ whitelist.js
@@ -0,0 +1,9 @@
+var whitelist = ['2ip.ru', 'myip.ru'];
+
+function FindProxyForURL(url, host) {
+  host = host.toLowerCase();
+  if (whitelist.includes(host))
+    return "SOCKS5 localhost:1080; SOCKS localhost:1080; DIRECT"; // (IP:port)
+
+  return "DIRECT";
+}
